efd9cadba18780421918422181bd0ab4337e2c7e
mikmc55 <80829378+mikmcdanbyeee55@users.noreply.github.com>
2025-01-30 14:39:10 +0000

    cbox

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const log = require('./src/helpers/logger');
-const routes = require('./src/routes/index');
+require("dotenv").config();
+import express, { static, json } from "express";
+import cors from "cors";
+import { join } from "path";
+import { info } from "./src/helpers/logger";
+import routes from "./src/routes/index";
 
 const PORT = process.env.PORT || 7000;
 const app = express();
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(static(join(__dirname, "public")));
 
-app.use(express.json());
+app.use(json());
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(PORT, () => {
-    log.info(`Server running on port ${PORT} - Environment: ${process.env.NODE_ENV || 'development'}`);
+  info(
+    `Server running on port ${PORT} - Environment: ${
+      process.env.NODE_ENV || "development"
+    }`
+  );
 });
